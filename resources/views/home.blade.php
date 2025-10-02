83e2a40654c7385edd8b04cf203856405e37ac1e
Dokii06 <zaldyseno06@gmail.com>
2025-10-03 06:14:47 +0700

    Mengupload file fpw pertemuan 3-5

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends ('master')
+
+@section ('title', 'Home Page')
+
+@section ('content')
+    <h1>Halo temen temennn, ini halaman home nya</h1>
+@endsection
---
+++ resources/views/layouts-percobaan/app.blade.php
@@ -0,0 +1,5 @@
+@extends ('master')
+
+@section ('content')
+    <h1>Halo temen temennn</h1>
+@endsection
